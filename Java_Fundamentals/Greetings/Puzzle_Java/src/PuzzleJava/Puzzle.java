a701ff745a1de9698a6ad62dadcb77fd18db341f
Ali Yahya <en1122al33@gmail.com>
2024-12-08 23:35:16 +0200

    puzzleJava

--- Java_Fundamentals/Greetings/Puzzle_Java/src/PuzzleJava/Puzzle.java
+++ Java_Fundamentals/Greetings/Puzzle_Java/src/PuzzleJava/Puzzle.java
@@ -1,17 +1,52 @@
 package PuzzleJava;
+
 import java.util.ArrayList;
 import java.util.Random;
 
-public class Puzzle{
-	
-	public ArrayList<Integer> gettenRolls(){
+public class Puzzle {
+
+	public ArrayList<Integer> gettenRolls() {
 		ArrayList<Integer> array = new ArrayList<>();
 		Random randNumber = new Random();
-		for(int i = 0 ; i < 10 ; i++) {
-			int rand = randNumber.nextInt(25)+5;//5->30
+		for (int i = 0; i < 10; i++) {
+			int rand = randNumber.nextInt(25) + 5;// 5->30
 			array.add(rand);
 		}
 		return array;
-		
 	}
+
+	public char getRandomLetter() {
+		char[] letter = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+		Random rand = new Random();
+		int randomIndex = rand.nextInt(26);
+		return letter[randomIndex];
+	}
+
+	public String generatePassword() {
+		String password = "";
+		for (int i = 0; i < 8; i++) {
+			password += getRandomLetter();
+		}
+		return password;
+	}
+
+	public ArrayList<String> getNewPasswordSet(int length) {
+		ArrayList<String> passwordSet = new ArrayList<>();
+		for (int i = 0; i < length; i++) {
+			passwordSet.add(generatePassword());
+		}
+		return passwordSet;
+	}
+	
+	public void  shulleArray(int[] array){
+		Random rand = new Random();
+		for (int i = 0 ; i < array.length ; i++) {
+			int randomIndex = rand.nextInt(array.length);
+			int temp = array[i];
+			array[i] = array[randomIndex];
+			array[randomIndex] = temp ; 
+			
+		}
+	}
+
 }
--- Java_Fundamentals/Greetings/Puzzle_Java/src/PuzzleJava/TestPuzzleJava.java
+++ Java_Fundamentals/Greetings/Puzzle_Java/src/PuzzleJava/TestPuzzleJava.java
@@ -1,11 +1,31 @@
 package PuzzleJava;
+
+import java.util.Array;
 import java.util.ArrayList;
+
 public class TestPuzzleJava {
 
 	public static void main(String[] args) {
 		Puzzle puzzle = new Puzzle();
 		ArrayList<Integer> array = puzzle.gettenRolls();
-		System.out.println("puzzle number is ="+" "+ array );
+		System.out.println("puzzle number is =" + " " + array);
+
+		char randomLetter = puzzle.getRandomLetter();
+		System.out.println("Random Letter: " + randomLetter);
+		
+		String password = puzzle.generatePassword();
+		System.out.println("Your new Password is:  "+ password);
+		
+		int numOfPassword = 8 ;
+		ArrayList<String> passwordNew = puzzle.getNewPasswordSet(numOfPassword);
+		System.out.println("Your new Password is:  "+ passwordNew);
+		
+		int[] arrayIndex = {10,20,30,49,29};
+		puzzle.shulleArray(arrayIndex);
+		for(int i = 0; i < arrayIndex.length;i++)
+			System.out.println(arrayIndex[i]);
+	    
+		
 	}
 
 }
